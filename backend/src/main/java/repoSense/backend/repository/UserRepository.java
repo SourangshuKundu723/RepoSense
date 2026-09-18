350a74f9e27b6c89b0f05aaa654dda16fd288a4f
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-09-18 19:59:49 +0530

    Add User entity, repository, and service for user management

---
+++ backend/src/main/java/repoSense/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package repoSense.backend.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import repoSense.backend.entity.User;
+
+public interface UserRepository extends JpaRepository<User, UUID>{
+    Optional<User> findByGithubId(Long githubId);
+}
